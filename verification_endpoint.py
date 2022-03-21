975b221b1da7e890225b8183a0a9e4b2a2b113d9
cleofan <58447038+cleofan@users.noreply.github.com>
2022-03-21 12:15:24 -0400

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,6 +11,9 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
+    payload = content.get('payload')
+    platform = payload.get('platform')
+    print("The platform is {}.\n".format(platform))
 
     #Check if signature is valid
     result = True #Should only be true if signature validates
