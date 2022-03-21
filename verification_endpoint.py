c9d051928838139d7831e62c50c97c9bc1fce2d7
cleofan <58447038+cleofan@users.noreply.github.com>
2022-03-21 12:51:43 -0400

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -17,12 +17,17 @@ def verify():
     payload = content.get('payload')
     platform = payload.get('platform')
     print("The platform is {}.\n".format(platform))
+    signature = content.get('sig')
+    print("The signature is {}.\n".format(signature))
+    message = payload.get('messgae')
+    print("The message send in payload is {}.\n".format(signature))
+    pk = payload.get('pk')
+    print("The public key is {}.\n".format(pk))
     
     #When the platform is algorand
     if (platform == "Algorand"):
-        signature = content.get('sig')
-        message = payload.get('message')
-        algo_pk = payload.get('pk')
+        print("Yo!\n")
+
         result = algosdk.util.verify_bytes(message.encode('utf-8'),signature,algo_pk)
         if (result):
             print("The message and signature verifies.\n")
