b84a9ed675885e04975a66c287e16815931304b2
cleofan <58447038+cleofan@users.noreply.github.com>
2022-03-21 15:42:54 -0400

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -12,32 +12,23 @@ app.config['DEBUG'] = True
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
-    
-    #First check which platform
     payload = content.get('payload')
-    #print(payload.keys())
     platform = payload.get('platform')
-    #print("The platform is {}.\n".format(platform))
     signature = content.get('sig')
-    #print("The signature is {}.\n".format(signature))
     message = payload.get('message')
-    #print("Message:", message.encode('utf-8'))
-    #test = "My Name Is Cleo"
-    #print("Test String:", test.encode('utf-8'))
-    #print("Message:", bytes(message, 'utf-8'))
     pk = payload.get('pk')
-    #print("The public key is {}.\n".format(pk))
+    result = False
     
     #When the platform is algorand
-    if (platform == 'Algorand'):
-        result = algosdk.util.verify_bytes(message.encode('utf-8'),signature, pk)
-        print("The message and signature verification is {}.\n".format(result))
+    if platform == 'Algorand':
+        if algosdk.util.verify_bytes(message.encode('utf-8'),signature, pk)
+            print("The message and signature verification is successful.")
+            result=True
     else:
         eth_encoded_msg = eth_account.messages.encode_defunct(text=message)
-        recovered_pk = eth_account.Account.recover_message(eth_encoded_msg, signature = signature)
-        print("The pk recovered is {}.\n".format(recovered_pk))
-        result = (recovered_pk == pk)
-        print("The verification result is:", result)
+        if eth_account.Account.recover_message(eth_encoded_msg, signature = signature) == pk,
+            result = True
+            print("The verification result is:", result)
 
     #Check if signature is valid
     #Should only be true if signature validates
